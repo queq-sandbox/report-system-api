397f60afc9942dd3f9c404d228b38924b5af70a5
queq1890 <32263803+queq1890@users.noreply.github.com>
2018-09-19 14:38:14 +0700

    define reports routings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :daily_reports, only: [:create, :update, :destroy]
+  resources :weekly_reports, only: [:create, :update, :destroy]
+
+  scope 'teams/:team_id', as: :team do
+    resources :daily_reports, module: :teams, only: :index
+    resources :weekly_reports, module: :teams, only: :index
+  end
 end
