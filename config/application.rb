47a33e613200a54259eb32bbbcacfcf24b3740a9
queq1890 <32263803+queq1890@users.noreply.github.com>
2018-09-21 10:19:23 +0700

    fix : add exposing keys

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module ReportSystemApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins "localhost:8080"
-        resource "*", headers: :any, methods: [:get, :post, :put, :delete, :options]
+        resource "*", headers: :any, expose: %w(access-token expiry token-type uid client), methods: [:get, :post, :put, :delete, :options]
       end
     end
 
