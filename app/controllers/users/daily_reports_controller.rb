b79fcbd088b5452a55388fdfb881cbef7f1bb619
Yuji Matsumoto <mtmtatmat4@toki.waseda.jp>
2018-10-25 16:21:21 +0700

    Revert "Add endpoints"

--- app/controllers/users/daily_reports_controller.rb
+++
@@ -1,8 +0,0 @@
-class Users::DailyReportsController < ApplicationController
-  before_action :authenticate_user!
-
-  def index
-    @daily_reports = DailyReport.where(user_id: params[:user_id])
-    render json: @daily_reports
-  end
-end
